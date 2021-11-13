6098019e96a072b0ce1adc8960fa21c273559269
Alex <alexjfuhr@gmail.com>
2021-11-12 22:34:54 -0500

    Added textures for tomamato plant

--- src/main/java/dev/alexjf/vanilla_improvements_agriculture/VanillaImprovementsAgriculture.java
+++ src/main/java/dev/alexjf/vanilla_improvements_agriculture/VanillaImprovementsAgriculture.java
@@ -13,6 +13,7 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -20,7 +21,7 @@ public class VanillaImprovementsAgriculture implements ModInitializer {
 	//variables
 	static String modid = "vanilla_improvements_agriculture";
 	//blocks
-	public static final PostBlock POST = new PostBlock(FabricBlockSettings.of(Material.WOOD).hardness(2f));
+	public static final PostBlock OAK_LOG_POST = new PostBlock(FabricBlockSettings.of(Material.WOOD).hardness(2f).sounds(BlockSoundGroup.WOOD));
 	//items
 	//nothing rn
 	//item groups
@@ -36,10 +37,10 @@ public class VanillaImprovementsAgriculture implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		//register blocks
-		Registry.register(Registry.BLOCK, new Identifier(modid, "post"), POST);
+		Registry.register(Registry.BLOCK, new Identifier(modid, "oak_log_post"), OAK_LOG_POST);
 		LOGGER.info("Finished block registry");
 		//register block items
-		Registry.register(Registry.ITEM, new Identifier(modid, "post"), new BlockItem(POST, new FabricItemSettings().group(VanillaImprovementsAgriculture.BLOCK_GROUP)));
+		Registry.register(Registry.ITEM, new Identifier(modid, "oak_log_post"), new BlockItem(OAK_LOG_POST, new FabricItemSettings().group(VanillaImprovementsAgriculture.BLOCK_GROUP)));
 		LOGGER.info("Finished block item registry");
 		//register items
 		LOGGER.info("Finished item registry");
