a679f034e91bb9587bcc7f23e8640e6018ae57cc
Alex <alexjfuhr@gmail.com>
2021-11-16 08:55:52 -0500

    testing with mixins

---
+++ src/main/java/dev/alexjf/vanilla_improvements_agriculture/mixin/FarmlandBlockMixin.java
@@ -0,0 +1,10 @@
+package dev.alexjf.vanilla_improvements_agriculture.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.block.FarmlandBlock;
+
+@Mixin(FarmlandBlock.class)
+public class FarmlandBlockMixin{
+	
+}
