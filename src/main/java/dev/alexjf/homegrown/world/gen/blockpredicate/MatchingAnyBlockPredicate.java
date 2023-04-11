d2a61e4b741a6127579dfe744f684cc0644c5d11
AlexJFDev <alexjfuhr@gmail.com>
2023-04-11 15:14:37 -0400

    Started matching any

--- src/main/java/dev/alexjf/homegrown/world/gen/blockpredicate/MatchingAnyBlockPredicate.java
+++ src/main/java/dev/alexjf/homegrown/world/gen/blockpredicate/MatchingAnyBlockPredicate.java
@@ -1,5 +1,24 @@
 package dev.alexjf.homegrown.world.gen.blockpredicate;
 
-public class MatchingAnyBlockPredicate {
+import com.mojang.datafixers.kinds.Applicative;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import net.minecraft.block.Block;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntryList;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.world.gen.blockpredicate.OffsetPredicate;
+
+public class MatchingAnyBlockPredicate 
+extends OffsetPredicate {
+    private final RegistryEntryList<Block> blocks;
+    private final RegistryEntryList<TagKey<Block>> blockTags;
+    private final RegistryEntryList<Fluid> fluids;
+    private final RegistryEntryList<TagKey<Fluid>> fluidTags;
+    public static final Codec<MatchingAnyBlockPredicate> CODEC = RecordCodecBuilder.create(instance -> MatchingAnyBlockPredicate.registerOffsetField(instance).and(((MapCodec)TagKey.unprefixedCodec(RegistryKeys.BLOCK).fieldOf("tag")).forGetter(predicate -> predicate.tag)).apply((Applicative<MatchingAnyBlockPredicate, ?>)instance, MatchingAnyBlockPredicate::new));
+    
     
 }
